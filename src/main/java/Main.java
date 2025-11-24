3bcae4b5b3a5ab9087ea158b2a2da215d61e755e
ILUNGA_KAYEMBE_GLAN <23ik069si@esisalama.org>
2025-11-24 16:09:19 +0100

    Modifier Document

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -68,7 +68,20 @@ void main() {
                 break;
             }
         } else if (choix == 3) {
-
+            ModifierDoc();
+            choix = ent.nextInt();
+            if (choix == 1){
+                ModifierLivre(repos);
+            } else if (choix == 2) {
+                ModifierJournal(repos);
+            } else if (choix == 3) {
+                ModifierMagazine(repos);
+            } else if (choix == 4) {
+                ModifierCd_dvd(repos);
+            }else {
+                System.out.println("Choix invalide.");
+                break;
+            }
         } else if (choix == 4) {
 
         } else if (choix == 5) {
@@ -149,8 +162,8 @@ public void AjoutJournal(DocumentListRepository repos){
     int mois = ent.nextInt();
     System.out.print("Jour : ");
     int jour = ent.nextInt();
-
     LocalDate dat = LocalDate.of(annee, mois, jour);
+
     Journal.add(new Journal( titre, auteur, code, annee_publication, nombre_exemplaire, disponibilite, dat));
 }
 
@@ -169,15 +182,15 @@ public void AjoutMagazine(DocumentListRepository repos){
     int nombre_exemplaire = ent.nextInt();
     System.out.println("Saisir la disponibilité du Magazine : ");
     String disponibilite = ent.nextLine();
-    System.out.println("Saisir le numero d'édition du Magazine : ");
+    System.out.println("Saisir  d'édition du Magazine : ");
     int numero_edition = ent.nextInt();
 
     Magazine.add(new Magazine(titre, auteur, code, annee_publication, nombre_exemplaire, disponibilite, numero_edition));
     repos.save(Magazine);
 }
 
-public void AjoutCd_dvd(DocumentListRepository repos){
-    List<Document>Cd_dvd = new ArrayList<>();
+public void AjoutCd_dvd(DocumentListRepository repos) {
+    List<Document> Cd_dvd = new ArrayList<>();
     Scanner ent = new Scanner(System.in);
     System.out.println("Saisir le titre du Disque : ");
     String titre = ent.next();
@@ -199,5 +212,135 @@ public void AjoutCd_dvd(DocumentListRepository repos){
 }
 
 public void ModifierDoc(){
+    System.out.println("Saisir type document à modifier : ");
+    System.out.println("1. Livre ");
+    System.out.println("2. Journal ");
+    System.out.println("3. Magazine ");
+    System.out.println("4. Cd_dvd ");
+}
 
+public void ModifierLivre(DocumentListRepository repos){
+    Scanner ent = new Scanner(System.in);
+    System.out.println("Saisir Titre du Livre : ");
+    String titre = ent.nextLine();
+
+    for (Document r : repos.load()){
+        if (r.getTitre() == titre || r instanceof Livre){
+            System.out.println("Saisir le titre du livre : ");
+            String Titre = ent.next();
+            System.out.println("Saisir le nom de l'auteur : ");
+            String auteur = ent.nextLine();
+            System.out.println("Saisir le code du livre : ");
+            int code = ent.nextInt();
+            System.out.println("Saisir l'année de publication du livre : ");
+            int annee_publication = ent.nextInt();
+            System.out.println("Saisir le nombre d'exemplaire du livre : ");
+            int nombre_exemplaire = ent.nextInt();
+            System.out.println("Saisir la disponibilité du livre : ");
+            String disponibilite = ent.nextLine();
+            System.out.println("Saisir le nombre de page du livre : ");
+            int nombre_page = ent.nextInt();
+
+            r = new Livre(Titre, auteur, code, annee_publication, nombre_exemplaire, disponibilite, nombre_page);
+            System.out.println("Journal modifier avec succes.");
+        }else {
+            System.out.println("Livre introuvable.");
+        }
+    }
+}
+
+public void ModifierJournal(DocumentListRepository repos){
+    Scanner ent = new Scanner(System.in);
+    System.out.println("Saisir Titre du Journal : ");
+    String titre = ent.nextLine();
+
+    for (Document d : repos.load()){
+        if (d instanceof Journal || d.getTitre() == titre){
+            System.out.println("Saisir le titre du Journal : ");
+            String Titre = ent.next();
+            System.out.println("Saisir le nom de l'auteur : ");
+            String auteur = ent.nextLine();
+            System.out.println("Saisir le code du Journal : ");
+            int code = ent.nextInt();
+            System.out.println("Saisir l'année de publication du Journal : ");
+            int annee_publication = ent.nextInt();
+            System.out.println("Saisir le nombre d'exemplaire du Journal : ");
+            int nombre_exemplaire = ent.nextInt();
+            System.out.println("Saisir la disponibilité du Journal : ");
+            String disponibilite = ent.nextLine();
+            System.out.println("Saisir la date de parution du Journal au format (YYYY-MM-dd): ");
+            System.out.print("Année : ");
+            int annee = ent.nextInt();
+            System.out.print("Mois : ");
+            int mois = ent.nextInt();
+            System.out.print("Jour : ");
+            int jour = ent.nextInt();
+            LocalDate dat = LocalDate.of(annee, mois, jour);
+
+            d = new Journal(Titre, auteur, code, annee_publication, nombre_exemplaire, disponibilite, dat);
+            System.out.println("Journal modifier avec succes.");
+        }else {
+            System.out.println("Journal introuvable.");
+        }
+    }
+}
+
+public void ModifierMagazine(DocumentListRepository repos){
+    Scanner ent = new Scanner(System.in);
+    System.out.println("Saisir Titre du Magazine : ");
+    String titre = ent.nextLine();
+
+    for (Document d : repos.load()){
+        if (d instanceof Magazine || d.getTitre() == titre){
+            System.out.println("Saisir le titre du Magazine : ");
+            String Titre = ent.next();
+            System.out.println("Saisir le nom de l'auteur : ");
+            String auteur = ent.nextLine();
+            System.out.println("Saisir le code du Magazine : ");
+            int code = ent.nextInt();
+            System.out.println("Saisir l'année de publication du Magazine : ");
+            int annee_publication = ent.nextInt();
+            System.out.println("Saisir le nombre d'exemplaire du Magazine : ");
+            int nombre_exemplaire = ent.nextInt();
+            System.out.println("Saisir la disponibilité du Magazine : ");
+            String disponibilite = ent.nextLine();
+            System.out.println("Saisir  d'édition du Magazine : ");
+            int numero_edition = ent.nextInt();
+
+            d = new Magazine(Titre, auteur, code, annee_publication, nombre_exemplaire, disponibilite, numero_edition);
+            System.out.println("Magazine Modifier avec succes.");
+        }else {
+            System.out.println("Magazine introuvable.");
+        }
+    }
+}
+
+public void ModifierCd_dvd(DocumentListRepository repos){
+    Scanner ent = new Scanner(System.in);
+    System.out.println("Saisir Titre du Cd_dvd : ");
+    String titre = ent.nextLine();
+
+    for (Document d : repos.load()){
+        if (d instanceof Cd_dvd || d.getTitre() == titre){
+            System.out.println("Saisir le titre du Disque : ");
+            String Titre = ent.next();
+            System.out.println("Saisir le nom de l'auteur : ");
+            String auteur = ent.nextLine();
+            System.out.println("Saisir le code du Disque : ");
+            int code = ent.nextInt();
+            System.out.println("Saisir l'année de publication du Disque : ");
+            int annee_publication = ent.nextInt();
+            System.out.println("Saisir le nombre d'exemplaire du Disque : ");
+            int nombre_exemplaire = ent.nextInt();
+            System.out.println("Saisir la disponibilité du Disque : ");
+            String disponibilite = ent.nextLine();
+            System.out.println("Saisir la durée du Disque : ");
+            double duree = ent.nextDouble();
+
+            d = new Cd_dvd(Titre, auteur, code, annee_publication, nombre_exemplaire, disponibilite, duree);
+            System.out.println("Cd_dvd modifier avec succes.");
+        }else {
+            System.out.println("CD_dvd introuvable.");
+        }
+    }
 }
